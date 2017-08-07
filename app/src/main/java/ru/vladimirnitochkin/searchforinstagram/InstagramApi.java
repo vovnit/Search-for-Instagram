32a7ac1b839d8bd263f9fe21f6bcf75d1ddfe5c1
Unknown <vovnit@gmail.com>
2017-08-07 16:17:25 +0300

    changed access_token

--- app/src/main/java/ru/vladimirnitochkin/searchforinstagram/InstagramApi.java
+++ app/src/main/java/ru/vladimirnitochkin/searchforinstagram/InstagramApi.java
@@ -15,7 +15,7 @@ import ru.vladimirnitochkin.searchforinstagram.pojo.Profile;
 
 public interface InstagramApi {
     String BASE_URL="https://api.instagram.com/";
-    String ACCESS_TOKEN="1414707315.3469706.3014ea70b5b04bc6aed3d47f4738f445";
+    String ACCESS_TOKEN="5845677537.447658c.fa592a283ebb4001b99311b371c85897";
     int PostsToLoad=5;
 
     @GET("v1/users/self/media/recent/")
